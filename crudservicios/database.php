9e92e87280d94281bf0c265dd4c99827a35660a3
Sebastian <jsruiz7411@misena.edu.co>
2020-10-03 17:02:34 -0500

    cambios

--- crudservicios/database.php
+++ crudservicios/database.php
@@ -1,10 +1,10 @@
 <?php
 class Database{
     public function getconectar(){
-    $localhost = "localhost";
-    $database = "trapitos";
-    $user = "root"; 
-    $password = "";
+    $localhost = 'bile850769ms7y36d0gt-mysql.services.clever-cloud.com';
+    $database = 'bile850769ms7y36d0gt';
+    $user = 'u7qyhwwkxjvyhcbj'; 
+    $password = 'vVfrQrSRySpfPdcYQhpS';
     $link = new PDO("mysql:host=$localhost; dbname=$database", $user, $password);
     return $link;
     }
